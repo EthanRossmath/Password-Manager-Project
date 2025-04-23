0baf514bada903ffb78da94822835b201bfebabd
EthanRossmath <ethanross981@gmail.com>
2025-04-23 18:37:51 -0400

    in inititialize.sh I modified the functions inititailize() and create_master_password() to create a master password if it does not exist

--- src/initialize.sh
+++ src/initialize.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 create_master_password() {
-	echo "Creating master password..."
+	while true; do
+	read -p "Enter master password: " MASTER_PASSWORD
+	read -p "Re-enter master password: " re_entered
+	
+	if [ $MASTER_PASSWORD != $re_entered ]
+	then
+	 echo "Passwords do not match, please try again"
+	else
+	 break
+	fi
+	done
+
+	echo $(openssl passwd -6 -salt $(openssl rand -base64 16) $MASTER_PASSWORD) > data/.MASTER
+
 }
 
 check_master_password() {
