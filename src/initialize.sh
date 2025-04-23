4a837c982efd9fc9600ccf0db913efad0b086e9a
EthanRossmath <ethanross981@gmail.com>
2025-04-23 17:57:34 -0400

    Added the three functions initialize(), create_masster_password(), and check_master_password() in initialize.sh

---
+++ src/initialize.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+create_master_password() {
+	echo "Creating master password..."
+}
+
+check_master_password() {
+	echo "Checking master password..."
+}
+
+initialize() {
+	if [ -e "data/.MASTER" ]
+	then
+	 check_master_password
+	else
+	 create_master_password
+	fi
+}
+
+initialize
+
+
