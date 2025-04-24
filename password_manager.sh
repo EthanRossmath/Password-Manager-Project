f4c669a6b04be470c446c63b6dd012c5f773fcc8
EthanRossmath <ethanross981@gmail.com>
2025-04-24 00:56:26 -0400

    added comments to password-manager.sh

--- password_manager.sh
+++ password_manager.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# source commands
 source src/initialize.sh
 source src/passwords.sh
 source src/utils.sh
 
+# the show menu command. Produces 4 options and allows the user to select one
 show_menu() {
 while true; do
 echo
@@ -17,13 +19,16 @@ echo
 read -p "Choose an option (1 - 4)" option
 
 case $option in
-	1)
-	    new_password "$MASTER_PASSWORD"
+	1) # calls the function new_password from src/passwords.sh with argument $MASTER_PASSWORD
+	   # this allows the user to input a account name and produces an encrypted 24 byte base 64 string
+	   # using aes-256-cbc with password $MASTER_PASSWORD
+	    new_password "$MASTER_PASSWORD" 
 	    ;; 
-        2) 
+        2) # calls the function retrieve_password from src/passwords.sh with argument $MASTER_PASSWORD. This
+	   # prompts the user to input an existing account name, then produces the plaintext of the password.
             retrieve_password "$MASTER_PASSWORD"
 	    ;; 
-        3) 
+        3) # calls the function list_accounts from src/utils.sh. This lists all existing accounts.
             list_accounts
 	    ;;
         4) 
@@ -59,11 +64,15 @@ echo
 done
 }
 main() {
-initialize
+# calls upon the function initialize from src/initialize.sh. This prompts the user to produce a master password if they 
+# have not already. Otherwise, if the user has already chosen a master password, it then prompts the user to provide the
+# master password and checks if the prompt agrees with the stored master password.
+	initialize
 
-while true; do
-	show_menu
-done
+# loop show_menu function until the user makes a selection.
+	while true; do
+		show_menu
+	done
 }
 
 #You may choose to later inlcude arguments to main.
